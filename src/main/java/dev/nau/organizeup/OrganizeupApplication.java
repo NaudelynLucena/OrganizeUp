1a2270d7831d45f4a71e11cd1fec89c078f1a15b
NaudelynLucena <naulunau@gmail.com>
2025-04-19 19:22:18 +0200

    Initialize Spring Boot project with basic configuration dependencies and applicationProperties

---
+++ src/main/java/dev/nau/organizeup/OrganizeupApplication.java
@@ -0,0 +1,13 @@
+package dev.nau.organizeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nau/organizeup/OrganizeupApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nau.organizeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
