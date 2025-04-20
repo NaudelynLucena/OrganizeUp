ee80a5fdec2cc49bd48d201cf2a6e69ce938998c
NaudelynLucena <naulunau@gmail.com>
2025-04-21 00:29:35 +0200

    feat: implement CustomUserDetailsService for user authentication

---
+++ src/main/java/dev/nau/organizeup/user/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package dev.nau.organizeup.user.service;
+
+import dev.nau.organizeup.user.model.User;
+import dev.nau.organizeup.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+            .withUsername(user.getEmail())
+            .password(user.getPassword())
+            .authorities("ROLE_" + user.getRole().name())
+            .build();
+    }
+}
