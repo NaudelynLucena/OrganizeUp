dd3ce05293618ac0ca140a737f70e4ea367c1920
NaudelynLucena <naulunau@gmail.com>
2025-04-25 20:46:10 +0200

    feat: create ChildLoginRequest DTO with accessCode field and its getter/setter

---
+++ src/main/java/dev/nau/organizeup/auth/dto/ChildLoginRequest.java
@@ -0,0 +1,13 @@
+package dev.nau.organizeup.auth.dto;
+
+public class ChildLoginRequest {
+    private String accessCode;
+
+    public String getAccessCode() {
+        return accessCode;
+    }
+
+    public void setAccessCode(String accessCode) {
+        this.accessCode = accessCode;
+    }
+}
