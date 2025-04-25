430da8f68e53717b8c13fef0cc10f83d4be8f43a
NaudelynLucena <naulunau@gmail.com>
2025-04-25 18:07:57 +0200

    feat: add findByGuardian method to UserRepository for retrieving users by guardian

--- src/main/java/dev/nau/organizeup/user/repository/UserRepository.java
+++ src/main/java/dev/nau/organizeup/user/repository/UserRepository.java
@@ -3,9 +3,12 @@ package dev.nau.organizeup.user.repository;
 import dev.nau.organizeup.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    List<User> findByGuardian(User guardian);
+
 }
