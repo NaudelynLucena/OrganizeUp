bfeadd8a19c4728c875e93d7efe6af0e8a647b95
NaudelynLucena <naulunau@gmail.com>
2025-04-26 13:53:58 +0200

    feat: add GroupController for group creation endpoint

---
+++ src/main/java/dev/nau/organizeup/group/controller/GroupController.java
@@ -0,0 +1,31 @@
+package dev.nau.organizeup.group.controller;
+
+import dev.nau.organizeup.group.dto.CreateGroupRequest;
+import dev.nau.organizeup.group.dto.GroupResponse;
+import dev.nau.organizeup.group.model.Group;
+import dev.nau.organizeup.group.service.GroupService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<GroupResponse> createGroup(@Valid @RequestBody CreateGroupRequest request,
+            Authentication authentication) {
+        String email = authentication.getName();
+        Group group = groupService.createGroup(request.getName(), email);
+        GroupResponse response = new GroupResponse(group.getId(), group.getName(), group.getJoinCode());
+        return ResponseEntity.ok(response);
+    }
+}
