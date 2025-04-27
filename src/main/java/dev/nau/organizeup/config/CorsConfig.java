d88cbc3db591e842cda61bbde687a2b1d7182e1a
NaudelynLucena <naulunau@gmail.com>
2025-04-27 12:38:42 +0200

    feat: update allowed methods in CORS configuration to include PATCH

--- src/main/java/dev/nau/organizeup/config/CorsConfig.java
+++ src/main/java/dev/nau/organizeup/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
