bbe4ba78ce570e0f00e0981da46c9391b5e6c0d6
NaudelynLucena <naulunau@gmail.com>
2025-04-27 14:37:17 +0200

    feat: add ResourceNotFoundException and handler in GlobalExceptionHandler

--- src/main/java/dev/nau/organizeup/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/nau/organizeup/exception/GlobalExceptionHandler.java
@@ -42,4 +42,9 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
 }
